d36f42826aa634b1592813220dbf9bdb17f71d38
JibbyKT <89538905+JibbyKT@users.noreply.github.com>
2022-08-06 14:25:21 +0800

    Before trying to import bigcalendar

--- frontend/nextjs/pages/index.js
+++ frontend/nextjs/pages/index.js
@@ -2,28 +2,33 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import ProTip from "../src/ProTip";
-import Link from "../src/Link";
-import Copyright from "../src/Copyright";
 
-import InputComponent from "../Components/InputComponent";
-import LengthComponent from "../Components/LengthComponent";
+import Link from "../src/Link";
+
+import Head from "next/head";
+
+import InputComponent from "../src/Components/InputComponent";
+import LengthComponent from "../src/Components/LengthComponent";
 
 export default function Index() {
   return (
     <>
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4 }}>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      <Container maxWidth="xl">
+        <Box sx={{ my: 2, mx: 4 }}>
           <Typography variant="h4" component="h1" gutterBottom>
             Next.js example
           </Typography>
           <Link href="/about" color="secondary">
             Go to the about page
           </Link>
+          <br></br>
+          <br></br>
+
           <InputComponent></InputComponent>
           <LengthComponent></LengthComponent>
-          <ProTip />
-          <Copyright />
         </Box>
       </Container>
     </>
