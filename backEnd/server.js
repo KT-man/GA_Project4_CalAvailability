96c32df8069e1b4fc83f364f678f33c60f080544
JibbyKT <89538905+JibbyKT@users.noreply.github.com>
2022-08-09 11:20:09 +0800

    merged

--- backEnd/server.js
+++ backEnd/server.js
@@ -9,9 +9,9 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors());
 connectDB(process.env.MONGODB_URI);
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
